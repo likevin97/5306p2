eec5daa8ba1d02f38f38d77ef680226a131d8d32
Charles Yu <charles.yu108@gmail.com>
2018-11-05 17:09:20 -0500

    Basic sentiment counts

--- airports.py
+++ airports.py
@@ -7,38 +7,62 @@ with open("Tweets.csv") as f:
 
 # Preprocess steps
 for tweet in data:
-    tweet["text"] = tweet["text"].lower()
+    tweet["text"] = tweet["text"]
 
 # Define airports (Top 20 according to Wikipedia)
 # https://en.wikipedia.org/wiki/List_of_the_busiest_airports_in_the_United_States
 airports = [
     # format: airport_code, major_city, [additional potential names ...]
-    ["ATL", "Atlanta"],
-    ["LAX", "Los Angeles"],
-    ["ORD", "Chicago"],
-    ["DFW", "Dallas"],
-    ["DEN", "Denver"],
-    ["JFK", "New York"],
-    ["SFO", "San Francisco"],
-    ["LAS", "Las Vegas"],
-    ["SEA", "Seattle"],
-    ["CLT", "Charlotte"],
-    ["EWR", "Newark"],
-    ["MCO", "Orlando"],
-    ["PHX", "Phoenix"],
-    ["MIA", "Miami"],
-    ["IAH", "Houston"],
-    ["BOS", "Boston"],
-    ["MSP", "Minneapolis"],
-    ["DTW", "Detroit"],
-    ["FLL", "Fort Lauderdale"],
-    ["PHL", "Philadelphia"],
+    ["ATL", "Atlanta", "atlanta"],
+    ["LAX", "Los Angeles", "los angeles"],
+    ["ORD", "Chicago", "chicago"],
+    ["DFW", "Dallas", "dallas"],
+    ["DEN", "Denver", "denver"],
+    ["JFK", "New York", "new york", "NYC", "nyc"],
+    ["SFO", "San Francisco", "san francisco", "SF", "sf"],
+    ["LAS", "Las Vegas", "las vegas"],
+    ["SEA", "Seattle", "seattle"],
+    ["CLT", "Charlotte", "charlotte"],
+    ["EWR", "Newark", "newark"],
+    ["MCO", "Orlando", "orlando"],
+    ["PHX", "Phoenix", "phoenix"],
+    ["MIA", "Miami", "miami"],
+    ["IAH", "Houston", "houston"],
+    ["BOS", "Boston", "boston"],
+    ["MSP", "Minneapolis", "minneapolis"],
+    ["DTW", "Detroit", "detroit"],
+    ["FLL", "Fort Lauderdale", "fort lauderdale"],
+    ["PHL", "Philadelphia", "philadelphia", "Philly", "philly"],
 ]
 
-def airportReferenceExists(text):
-    return any(map(lambda names: any([name.lower() in text for name in names]), airports))
+def airportReferences(text):
+    """Returns a list of all the airports detected in the tweet"""
+    refs = map(lambda names: names[0] if \
+                    any([name in text for name in names]) \
+                    else None, airports)
+    return list(filter(lambda x: x is not None, refs))
 
-filt = [tweet for tweet in data if airportReferenceExists(tweet["text"])]
+all_filt_tweets = []
+airport_tweets = {airport[0] : [] for airport in airports}
+for tweet in data:
+    refs = airportReferences(tweet["text"])
+    if len(refs):
+        [airport_tweets[r].append(tweet) for r in refs]
+        all_filt_tweets.append(tweet)
 
+# Some print statements for sanity checks
 print("Prior to filtering, data points:", len(data))
-print("After filtering:", len(filt))
+print("After filtering:", len(all_filt_tweets))
+print()
+for k,v in airport_tweets.items():
+    print(k, len(v))
+    print("First tweet: ", v[0]['text'])
+    print()
+
+print("====== Sentiment Counts ======")
+for airport_code, tweets in airport_tweets.items():
+    sents = list(map(lambda twt: twt["airline_sentiment"], tweets))
+    pos = list(filter(lambda x: x == "positive", sents))
+    neg = list(filter(lambda x: x == "negative", sents))
+    neut = list(filter(lambda x: x == "neutral", sents))
+    print(airport_code, "[ pos:", len(pos), "// neg:", len(neg), "// neut:", len(neut), "]")
